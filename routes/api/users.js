47f5af8b464529f562726463cd204a2b71119f9f
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-09 11:37:57 -0700

    routes: add user registration
    Add User model registration as POST request to '/api/users/register'

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
-// @route   GET api/users/test
-// @desc    Tests users route
+// Load User model
+const User = require('../../models/User');
+
+// @route   POST api/users/register
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.json({ msg: 'User' }));
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    }
+
+    const newUser = new User({
+      name: req.body.name,
+      nickname: req.body.nickname,
+      email: req.body.email,
+      avatar: gravatar.url(req.body.email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      }),
+      password: req.body.password
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) {
+          throw err;
+        }
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err));
+      });
+    });
+  });
+});
 
 module.exports = router;
