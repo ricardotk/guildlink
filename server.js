6115532da2341f4cc58ce76c32ec17ce9a14ceea
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-09 12:22:07 -0700

    server: add passport jwt authentication strategy
    This patch adds passport json web token authentication strategy
    that is going to check for the jwt token and return the current
    user, in order to keep the user logged in.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Passport middleware
+app.use(passport.initialize());
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,7 +28,8 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('GuildLink'));
+// Passport Config
+require('./config/passport.js')(passport);
 
 // Use Routes
 app.use('/api/users', users);
