94ba3cd9bdb68ab04a7f2ac636ef06ffb27ac896
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-09 12:25:00 -0700

    routes: add route to get current user
    This patch adds aroute to get the current user based on the jwt
    token.
    
    This route is for backend test purposes.

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 // Load User model
 const User = require('../../models/User');
@@ -81,5 +82,21 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      nickname: req.user.nickname,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
