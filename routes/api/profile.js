c3735d90b2f71010108f26d4df9096432cd8abab
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-10 00:55:04 -0700

    routes: add create/update user profile route
    This patch adds a route to create/update the current user profile
    using the jwt token, POST request to '/api/profile'

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -30,4 +30,53 @@ router.get(
   }
 );
 
+// @route   POST api/profile
+// @desc    Create or Edit user profile
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.discord) profileFields.discord = req.body.discord;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    // Interests - Split into array
+    if (typeof req.body.interests !== 'undefined') {
+      profileFields.interests = req.body.interests.split(',');
+    }
+
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update
+        return Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      }
+
+      // Create
+      Profile.findOne({ handle: profileFields.handle }).then(profile => {
+        if (profile) {
+          errors.handle = 'Handle already exists';
+          res.status(400).json(errors);
+        }
+
+        new Profile(profileFields).save().then(profile => res.json(profile));
+      });
+    });
+  }
+);
+
 module.exports = router;
