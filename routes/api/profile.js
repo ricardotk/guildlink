e0110b4b6854db7d1480589de88d0ae48fe5b0b8
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-10 11:27:46 -0700

    routes: add get all user profiles route
    This patch adds a route to get all user profiles, GET request to
    '/api/profile/all'

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -33,6 +33,25 @@ router.get(
   }
 );
 
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (profiles) {
+        return res.json(profiles);
+      }
+
+      errors.noprofile = 'There are no profiles';
+      res.status(404).json(errors);
+    })
+    .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+});
+
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
