2183dac37129953b80e16f5811a9232805e208c4
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-10 13:10:54 -0700

    routes: add delete user and profile route
    This patch adds a route to delete user and profile using jwt token,
    DELETE request to '/api/profile'

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const passport = require('passport');
 
 // Load Validation
@@ -195,6 +194,23 @@ router.delete(
         }
 
         profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+      .then(() => {
+        User.findOneAndRemove({ _id: req.user.id })
+          .then(() => res.json({ success: true }))
+          .catch(err => res.status(404).json(err));
       })
       .catch(err => res.status(404).json(err));
   }
