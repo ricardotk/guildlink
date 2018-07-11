61eb2ec61ceba98fbe770be860a1e2652e32945a
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-11 12:49:24 -0700

    layout: add Navbar react component
    This patch adds the Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
 import './App.css';
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Landing />
       </div>
     );
