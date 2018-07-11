bbcb46bd4c28d925335650b23892df5c232afdd1
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-11 14:20:44 -0700

    layout: start using Link component instead of <a>
    This patch changes all <a> to <Link> component in order to use
    react router dom

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
@@ -14,12 +15,12 @@ class Landing extends Component {
                   other guild members
                 </p>
                 <hr />
-                <a href="" className="btn btn-lg btn-info mr-2">
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
-                </a>
-                <a href="" className="btn btn-lg btn-light">
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
                   Login
-                </a>
+                </Link>
               </div>
             </div>
           </div>
