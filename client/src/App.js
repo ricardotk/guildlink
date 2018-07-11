013b489b8fa8f7c031750b224c9c220353e09100
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-11 14:01:14 -0700

    client: start using react-router-dom
    This patch adds basic infrastructure to App.js such as BrowserRouter
    and Route from react-router-dom package.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -9,11 +10,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
