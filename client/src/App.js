39323817e73f26b0a6776aeb6dd6351942cbf196
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-11 19:48:48 -0700

    client: add the login form component with state
    This patch adds the login form component with state

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
 
@@ -17,6 +18,7 @@ class App extends Component {
           <Route exact path="/" component={Landing} />
           <div className="container">
             <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
           </div>
           <Footer />
         </div>
