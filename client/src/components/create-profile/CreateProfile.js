46d2fb818e047c3386d4919f18875d9739614668
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-14 17:15:17 -0700

    client: add functionality to CreateProfile component
    This patch adds functionality to CreateProfile component by
    calling createProfile action.

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
+import { createProfile } from '../../actions/profileActions';
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -28,6 +30,12 @@ class CreateProfile extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
 
@@ -44,7 +52,7 @@ class CreateProfile extends Component {
       instagram: this.state.instagram
     };
 
-    console.log(profileData);
+    this.props.createProfile(profileData, this.props.history);
   }
 
   onChange(e) {
@@ -198,4 +206,7 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(
+  mapStateToProps,
+  { createProfile }
+)(withRouter(CreateProfile));
