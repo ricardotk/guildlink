553c20c49d221ce6e56fc956aee5b6df0950a5b4
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-15 16:41:54 -0700

    client: add Profiles component
    This patch adds the Profiles component that is going to show all
    the profiles in one page. Right now we are getting the data using
    getProfiles action and keeping in redux. Next steps are going to be
    to create ProfileItem component and show all components here using it.

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddCharacter from './components/add-character/AddCharacter';
+import Profiles from './components/profiles/Profiles';
 
 import './App.css';
 
@@ -58,6 +59,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
