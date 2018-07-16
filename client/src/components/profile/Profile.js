deec5126ba6b7b2159582e3acc85ac78156b0585
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-16 14:27:46 -0700

    client: add ProfileHeader component
    This patch adds ProfileHeader component that is going to be used
    in Profile component to the user header information that contains
    name, nickname, location and social medias.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Spinner from '../common/Spinner';
+import ProfileHeader from './ProfileHeader';
 import { getProfileByHandle } from '../../actions/profileActions';
 
 class Profile extends Component {
@@ -11,7 +14,36 @@ class Profile extends Component {
   }
 
   render() {
-    return <div />;
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back to Profiles
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+        </div>
+      );
+    }
+
+    return (
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
