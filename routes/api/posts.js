b6951fb27936083868e185324090a6b91b0b000d
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-17 12:55:19 -0700

    routes: add get posts route
    This patch adds a route to get all posts, GET request to '/api/posts'

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,6 +9,16 @@ const validatePostInput = require('../../validation/post');
 // Post model
 const Post = require('../../models/Post');
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
