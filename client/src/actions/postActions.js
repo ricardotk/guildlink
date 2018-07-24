79d8b89e1aa3229a909f8299a3079116f579b156
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-24 12:55:23 -0700

    client: add removeLike action
    This patch adds removeLike action that is going to be used
    in PostItem component to unlike a post in the post feed.

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -76,6 +76,19 @@ export const addLike = id => dispatch => {
     );
 };
 
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Set loading state
 export const setPostLoading = () => {
   return {
