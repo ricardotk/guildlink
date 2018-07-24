4a288a46834a3be4962aec1378b24df304c702a3
Ricardo de Almeida Gonzaga <xricardotk@gmail.com>
2018-07-24 12:18:28 -0700

    client: create addLike action
    This patch creates addLike action that is going to be used
    in PostItem component to like a post in the post feed.

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -63,6 +63,19 @@ export const deletePost = id => dispatch => {
     );
 };
 
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Set loading state
 export const setPostLoading = () => {
   return {
